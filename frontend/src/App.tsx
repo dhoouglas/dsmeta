959e57cfd43d2ff6cb2c4f5eaa0259bd3c773b36
unknown <dhoouglas@hotmail.com>
2022-07-12 21:39:00 -0300

    Other Component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,19 @@
-import NotificationButton from "./components/NotificationButton"
+import Header from "./components/Header"
+import SalesCard from "./components/SalesCard"
+
 
 function App() {
   return (
     <>
-      <h1>Hello Wolrd!</h1>
-      <NotificationButton />
-      <NotificationButton />
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
     </>
-    
   )
 }
 
